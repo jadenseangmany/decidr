1ad3c0898841c40faa6426e4b941cc8acc070b9e
tsitu0 <tomsitu0102@gmail.com>
2025-12-25 22:16:24 -0800

    Simple JWT Implementation

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express';
 import * as userController from '../controllers/userController';
-
+import {requireAuth} from '../middleware/requireAuth';
 const router = express.Router();
 
-router.get('/:username', userController.getUserInfo);
+router.get('/:username',requireAuth, userController.getUserInfo);
 
 router.post('/signup', userController.userSignup);
 
@@ -23,4 +23,18 @@ small json test for postman
   "password": "password123",
   "email": "test@example.com"
 }
+*/
+
+/*
+Signup
+curl -X POST http://localhost:3000/users/signup \
+  -H "Content-Type: application/json" \
+  -d '{"username":"testuser","password":"password123","email":"test@example.com"}'
+Login & Grab Token
+curl -X POST http://localhost:3000/users/login \
+  -H "Content-Type: application/json" \
+  -d '{"username":"testuser","password":"password123"}'
+Check
+curl http://localhost:3000/users/testuser \
+  -H "Authorization: Bearer <PASTE_TOKEN_HERE>"
 */
